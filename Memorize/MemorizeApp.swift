e3ec6290ca41e56554faaa315a93a967a8499b4f
Seab Jackson <seabjackson@gmail.com>
2023-12-27 20:05:11 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Seab Jackson on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
